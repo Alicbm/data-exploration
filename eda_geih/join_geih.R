d8b3790fd75b99e71bd69b55b65a762e2b6cbbcb
Alic Barandica (university) <abarandica1234@gmail.com>
2024-08-21 19:38:34 -0500

    funcion para el pegado completo de la geih y modificacion en la funcion para el pegado de cada mes

--- eda_geih/join_geih.R
+++ eda_geih/join_geih.R
@@ -5,7 +5,7 @@ merge_month <- function(month) {
   
   final_df <- data.table()
   
-  key_variables = c("DIRECTORIO", "SECUENCIA_P", "ORDEN","HOGAR", "FEX_C18", "PERIODO", "MES", "PER", "REGIS", "AREA", "DPTO", "CLASE")
+  key_variables = c("DIRECTORIO", "SECUENCIA_P", "ORDEN","HOGAR", "FEX_C18")
   
   setwd(paste0("C:/data_exploration/data-exploration/eda_geih/datos/", month))
   
@@ -23,6 +23,10 @@ merge_month <- function(month) {
     new_key_variables <- col_names[col_names %in% key_variables]
     
     final_df <- merge(final_df, df, by = c(new_key_variables), all.x = T, no.dups = TRUE)
+    
+    if ("PERIODO.x" %in% colnames(final_df)) {
+      final_df[, c("PERIODO.x", "MES.x", "PER.x", "REGIS.x", "AREA.x", "CLASE.x", "DPTO.x", "PERIODO.y", "MES.y", "PER.y", "REGIS.y", "AREA.y", "CLASE.y", "DPTO.y") := NULL]
+    }
   }
   
   return (final_df)
@@ -31,6 +35,32 @@ merge_month <- function(month) {
 
 
 
+#####################################################
 
+geih_completed <- function () {
+
+  months <- c("enero", "febrero", "marzo", "abril", "mayo", "junio")
+  
+  all_months <- data.table()
+
+  for (month in months) {
+    
+    if (length(all_months) == 0) {
+      all_months <- merge_month(month)
+    } else {
+      all_months <- rbind(all_months, merge_month(month))
+      
+    }
+    
+  }
+  
+  return (all_months)
+     
+}
+
+data <- geih_completed()
+
+
+#####################################################
 
 
