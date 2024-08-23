de280fddeff5de77e1f34e3f905e1248be5e464d
Alic Barandica (university) <abarandica1234@gmail.com>
2024-08-23 08:22:50 -0500

    funciones optimizadas (merge_month, geih_completed) , rutas relativas implementadas

---
+++ join_geih.R
@@ -0,0 +1,73 @@
+library(data.table)
+
+merge_month <- function(month) {
+  
+  variables_to_delete <- c("PERIODO", "MES", "PER", "REGIS", "AREA", "CLASE", "DPTO")
+  key_variables <- c("DIRECTORIO", "SECUENCIA_P", "ORDEN", "HOGAR", "FEX_C18")
+  
+  base_dir <- file.path(getwd(), "datos", month)
+  
+  all_files <- list.files(path = base_dir, pattern = "*.csv", full.names = TRUE, ignore.case = TRUE)
+  
+  final_df <- fread(file = all_files[1])
+  
+  for (file in all_files[-1]) {
+    df <- fread(file = file)
+    
+    new_key_variables <- intersect(colnames(df), key_variables)
+    
+    final_df <- merge(final_df, df, by = new_key_variables, all.x = TRUE)
+    
+    if ("PERIODO.x" %in% colnames(final_df)) {
+      setnames(final_df, old = paste0(variables_to_delete, ".x"), new = gsub("\\.x$", "", variables_to_delete))
+      final_df[, c(paste0(variables_to_delete, ".y")) := NULL]
+    }
+  }
+  
+  return(final_df)
+}
+
+
+geih_completed <- function () {
+
+  base_dir <- file.path(getwd(), "datos")
+  
+  months <- list.dirs(path = base_dir, full.names = FALSE, recursive = FALSE)
+  
+  all_months <- data.table()
+
+  for (month in months) {
+    
+    if (length(all_months) == 0) {
+      all_months <- merge_month(month)
+    } else {
+      all_months <- rbindlist(list(all_months, merge_month(month)), fill = T)
+      
+    }
+    
+  }
+  
+  return (all_months)
+     
+}
+
+data <- geih_completed()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
